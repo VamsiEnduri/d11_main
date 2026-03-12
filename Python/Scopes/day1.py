d5d85f772cc45b5b240e56e9b513c27cf7c4474b
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2026-03-12 08:42:31 +0530

    re_lo_dash & scopes global + function scopes

---
+++ Python/Scopes/day1.py
@@ -0,0 +1,118 @@
+a=10
+print(a)
+
+# scope :--the area in which you access a varaible is called as scope
+# what is scope of varaible a in line 1:-- ans :-- total entire file 
+# why ? global varaible :-- globally accessable
+
+
+
+def add():
+    print(a)
+    x=10 # the scope of x is ? only in the block of add function
+    y=20
+    print()
+    print(x+y)
+add()    
+print()
+
+def mul():
+    print(a)
+    a=100 # in mul function block itself
+    b=2 # mul func block only :-- scope
+    print(a*b)
+mul()
+
+# what is the function scoped variable here in the file ? mention any two ?
+
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(a)
---
+++ Python/functions/day3.py
@@ -0,0 +1,49 @@
+# # def abc(k,l,name,loc,surName="enduri"):
+# #     print(surName)
+# #     print(k,l,name,loc)
+
+# # abc(1,2,name="vamsi",loc="hyd")
+# # # default params
+
+
+# # register
+# # login
+# # dashboard
+
+# name=input("enter yr name @register:-- ") # vamsi
+# email=input("enter yr email @register :-- ") # "vamsi@gmail.com"
+# password=input("enter yr password @register:-- ")
+# c_password=input("enter yr c_password  @register:-- ")
+
+
+# def dashboard(n):
+#     print(f"welcome {n}")
+
+
+# def login(em,pa,na):
+#     emailL=input("enter yr email @login :-- ") # "vamsi@gmail.com"
+#     passwordL=input("enter yr password @login:-- ")
+#     # db r_users get
+#     # [{},{}]
+#     # for iterate
+#     # emailL=email 
+#     # break
+#     if emailL == em and passwordL == pa:
+#         dashboard(na)
+#     else:
+#         print("invalid credentials")
+
+
+
+
+
+
+# def register(n,e,p,cp):
+#     # n,p
+#     if p == cp :
+#         login(e,p,n)
+#     else:
+#         print("password & c-password should be same")    
+# register(name,email,password,c_password)    
+
+
