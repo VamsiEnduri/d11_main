e8d77865eb7dabc29474e116c75487577be4fbc5
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2026-03-18 10:40:44 +0530

    for loop-day-2

---
+++ Python/for_loop/day2.py
@@ -0,0 +1,87 @@
+# # reverse a str r list
+# a="marvel"
+# rev="levram"
+
+# inp=input("enter some str here :-- ") #apple inp[4]
+# # l=len(inp) #5
+
+# for i in range(len(inp)-1,-1,-1): # i=3
+#     print(inp[i],end="")# inp[3] :-- l
+# # for i in inp:
+# #     print(i)
+
+
+
+# inp=input("enter some str here :-- ") #apple inp[4]
+# rev="" # "" + inp[i] "e"
+#        # "e" + inp[i] :-- "elppa"
+# for i in inp:
+#     rev=i+rev 
+# print(rev,"revStr")    
+
+
+
+# abc=["banana","apple","orange","kiwi"]
+# # pqr=[5,4,3,2,1]
+# res=[]
+# for i in range(len(abc)-1,-1,-1): #i=2
+#     if len(abc[i])>5:  # if len(abc[2]) :-- if 6>=5
+#         res.append(abc[i]) # ["orange"]
+# print(res,"revList")    
+
+
+
+# # remove duplicates in str r list
+
+# a="applellleeeeaaaaaaaazxcv"
+# # op="aple"
+# op="" # "ap"
+# for i in a: #i=a
+#     if i not in op : # if "p" not in "ap"
+#         op+=i
+# print(op,"unique")        
+
+#  list :--  removing duplicates
+
+# abc=[1,2,3,4,1,1]
+# res=[]
+
+# for i in abc:
+#     if i not in res:
+#         res.append(i)
+
+# print(res,"unique list")
+# pqr=[1,2,3,4]
+
+
+# # find occurance of the items in str r list
+abc=[1,2,2,1,2,3,4]
+res={}
+for i in abc: # i=3
+    if i not in res: #i=3 not in res{}
+        res[i]=1  # res={"1":1,"2":1,"3":1,"4":1}
+    else:
+        res[i]+=1    
+print(res)
+# a={"id":1,"name":"vamsi"}
+
+# for i in a:
+#     if i not in a:
+#         print("hello")
+
+
+# a={
+#     "id":1
+# }
+
+# a["name"] ="vamsi" #
+# print(a)
+# pqr={"1":2,"2":3,"3":1,"4":1}
+
+# s="apple"
+# pqr={"a":1,"p":2,"l":1,"e":1}
+
+
+
+# # python qns untill now covered topics 
+
